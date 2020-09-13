bdbc702a6933ae15f801f0a667b26330f228cb35
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-09-13 16:24:47 -0300

    Update app.js

--- ejercicio_3/app.js
+++ ejercicio_3/app.js
@@ -3,6 +3,14 @@ const obtenerPokemon = require("./library");
 
 // Codigo funcion callback
 
+function callback(datosPokemon) {
+    console.log("Nombre: " + datosPokemon.forms[0].name);
+
+    console.log("Habilidades: ");
+    
+    datosPokemon.abilities.forEach
+    (ability => console.log(ability.ability.name));
+}
 
 
 // Fin codigo
@@ -11,4 +19,4 @@ const obtenerPokemon = require("./library");
 // - nombrePokemon es un string con el nombre del pokemon
 // - funcionCallback es una función que recibe el objeto con los datos del pokemon extraido
 
-obtenerPokemon();
+obtenerPokemon("raticate", callback);
