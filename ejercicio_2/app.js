d6b69bbc4ab3c8b9a281f8e78790e3df37878c5d
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-09-17 19:46:10 -0300

    .

--- ejercicio_2/app.js
+++ ejercicio_2/app.js
@@ -2,4 +2,5 @@
 const obtenerChiste = require("./library");
 
 // Usar la funcion obtenerChiste() la cual devuelve la promesa de traer el objeto chiste extraido
-obtenerChiste().then(res=>console.log(res));
+obtenerChiste().then(obj=>console.log(obj[0].setup));
+obtenerChiste().then(obj=>console.log(obj[0].punchline));
