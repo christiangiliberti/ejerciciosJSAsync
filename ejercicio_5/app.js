7ec7e3d2f948e11a0f3c0d8f10b14821702b3ba9
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-09-17 20:36:30 -0300

    .

--- ejercicio_5/app.js
+++ ejercicio_5/app.js
@@ -2,17 +2,17 @@ const obtenerPersonaFake = require("./library");
 
 // Codigo funcion callback
 function callback(persona) {
-    persona.results.forEach(person=> {
-        console.log("Nombre: " + person.name.first + " " + person.name.last);
-        console.log("Genero: " + person.gender);
-        console.log("Pais: " + person.location.country);
-        console.log("Estado: " + person.location.state); 
-        console.log("Calle: " + person.location.street.name);
-        console.log("Numero: " + person.location.street.number);
-        console.log("Codigo postal: " + person.location.postcode);
-        console.log("Email: " + person.email);
-        console.log("Usuario: " + person.login.username);
-        console.log("Password: " + person.login.password);
+    persona.results.forEach(i=> {
+        console.log("Nombre: " + i.name.first + " " + i.name.last);
+        console.log("Genero: " + i.gender);
+        console.log("Pais: " + i.location.country);
+        console.log("Estado: " + i.location.state); 
+        console.log("Calle: " + i.location.street.name);
+        console.log("Numero: " + i.location.street.number);
+        console.log("Codigo postal: " + i.location.postcode);
+        console.log("Email: " + i.email);
+        console.log("Usuario: " + i.login.username);
+        console.log("Password: " + i.login.password);
     })
 }
 
--- ejercicio_6/app.js
+++ ejercicio_6/app.js
@@ -3,16 +3,16 @@ const obtenerPersonaFake = require("./library");
 
 // Usar la funcion obtenerPersonaFake() la cual devuelve la promesa de traer el objeto persona extraido
 obtenerPersonaFake().then(person => {
-    person.results.forEach(person => {
-        console.log("Nombre: " + person.name.first + " " + person.name.last);
-        console.log("Genero: " + person.gender);
-        console.log("Pais: " + person.location.country);
-        console.log("Estado: " + person.location.state); 
-        console.log("Calle: " + person.location.street.name);
-        console.log("Numero: " + person.location.street.number);
-        console.log("Codigo postal: " + person.location.postcode);
-        console.log("Email: " + person.email);
-        console.log("Usuario: " + person.login.username);
-        console.log("Password: " + person.login.password);
+    person.results.forEach(i => {
+        console.log("Nombre: " + i.name.first + " " + i.name.last);
+        console.log("Genero: " + i.gender);
+        console.log("Pais: " + i.location.country);
+        console.log("Estado: " + i.location.state); 
+        console.log("Calle: " + i.location.street.name);
+        console.log("Numero: " + i.location.street.number);
+        console.log("Codigo postal: " + i.location.postcode);
+        console.log("Email: " + i.email);
+        console.log("Usuario: " + i.login.username);
+        console.log("Password: " + i.login.password);
     })
 });
