31ae71b6b8c947220a65648abe83cf4958680277
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-09-17 20:22:58 -0300

    .

--- ejercicio_5/app.js
+++ ejercicio_5/app.js
@@ -1,14 +1,13 @@
-
 const obtenerPersonaFake = require("./library");
 
 // Codigo funcion callback
 function callback(Persona) {
-    Persona.results.forEach(person => {
+    Persona.results.forEach(person=> {
 
         console.log("Nombre: " + person.name.first + " " + person.name.last);
         console.log("Genero: " + person.gender);
         console.log("Pais: " + person.location.country);
-        console.log("Estado: " + person.location.state ); 
+        console.log("Estado: " + person.location.state); 
         console.log("Calle: " + person.location.street.name);
         console.log("Numero: " + person.location.street.number);
         console.log("Codigo postal: " + person.location.postcode);
