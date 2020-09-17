ed25f5a5d4f6e4d3050b73b7515df4c418cf3be8
christiangiliberti <69557693+christiangiliberti@users.noreply.github.com>
2020-09-17 20:20:23 -0300

    .

--- ejercicio_4/app.js
+++ ejercicio_4/app.js
@@ -4,6 +4,5 @@ const obtenerPokemon = require("./library");
 // Usar la funcion obtenerChiste(nombrePokemon) la cual recibe como string un nombre de pokemon y devuelve la promesa de traer el objeto pokemon extraido
 obtenerPokemon("raticate").then(dPokemon => {
     console.log("Nombre: " + dPokemon.forms[0].name);
-    console.log("Habilidades: ");
-    dPokemon.abilities.forEach(h => console.log(h.ability.name));
+    dPokemon.abilities.forEach(h => console.log("Habilidad: " + h.ability.name));
 });
--- ejercicio_5/app.js
+++ ejercicio_5/app.js
@@ -2,14 +2,16 @@
 const obtenerPersonaFake = require("./library");
 
 // Codigo funcion callback
-function callback(dataPerson) {
-    dataPerson.results.forEach(person => {
+function callback(Persona) {
+    Persona.results.forEach(person => {
 
         console.log("Nombre: " + person.name.first + " " + person.name.last);
         console.log("Genero: " + person.gender);
-        console.log("Pais: " + person.location.country + ", Estado: " + person.location.state + 
-                    ", Calle: " + person.location.street.name + ", Numero: " + person.location.street.number +
-                    ", Codigo postal: " + person.location.postcode);
+        console.log("Pais: " + person.location.country);
+        console.log("Estado: " + person.location.state ); 
+        console.log("Calle: " + person.location.street.name);
+        console.log("Numero: " + person.location.street.number);
+        console.log("Codigo postal: " + person.location.postcode);
         console.log("Email: " + person.email);
         console.log("Usuario: " + person.login.username);
         console.log("Password: " + person.login.password);
